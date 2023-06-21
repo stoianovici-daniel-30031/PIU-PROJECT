f2dbd2aed0dfcc5544c0f65d60581e3101f35ef0
unknown <danielstoianovici218@yahoo.com>
2023-06-21 20:15:15 +0300

    -update localstorage saving items

--- src/Community.js
+++ src/Community.js
@@ -4,18 +4,13 @@ import './Community.css';
 const Community = () => {
   const [communityData, setCommunityData] = useState([]);
 
-  // Fetch initial data from localStorage when the component mounts
   useEffect(() => {
-    const savedData = localStorage.getItem('communityData');
-    if (savedData) {
-      setCommunityData(JSON.parse(savedData));
+    // Load data from localStorage on component mount
+    const storedData = localStorage.getItem('communityData');
+    if (storedData) {
+      setCommunityData(JSON.parse(storedData));
     }
   }, []);
-
-  // Update localStorage whenever communityData changes
-  useEffect(() => {
-    localStorage.setItem('communityData', JSON.stringify(communityData));
-  }, [communityData]);
 
   const [editingId, setEditingId] = useState(null);
   const [newPostTitle, setNewPostTitle] = useState('');
@@ -24,6 +19,7 @@ const Community = () => {
   const handleDelete = (postId) => {
     const updatedData = communityData.filter((post) => post.id !== postId);
     setCommunityData(updatedData);
+    localStorage.setItem('communityData', JSON.stringify(updatedData));
   };
 
   const handleEdit = (postId) => {
@@ -38,6 +34,7 @@ const Community = () => {
       return post;
     });
     setCommunityData(updatedData);
+    localStorage.setItem('communityData', JSON.stringify(updatedData));
     setEditingId(null);
     setNewPostTitle('');
     setNewPostContent('');
@@ -56,62 +53,68 @@ const Community = () => {
       content: newPostContent,
     };
     setCommunityData([...communityData, newPost]);
+    localStorage.setItem(
+      'communityData',
+      JSON.stringify([...communityData, newPost])
+    );
     setNewPostTitle('');
     setNewPostContent('');
   };
 
   return (
-    <div className= "background-set">
-    <div className="community-container">
-      <h2>Community Content</h2>
-      <div className="posts-container">
-        {communityData.map((post) => (
-          <div key={post.id} className="post">
-            {editingId === post.id ? (
-              <div className="edit-post">
-                <input
-                  type="text"
-                  value={newPostTitle}
-                  onChange={(e) => setNewPostTitle(e.target.value)}
-                  placeholder="Title"
-                />
-                <textarea
-                  value={newPostContent}
-                  onChange={(e) => setNewPostContent(e.target.value)}
-                  placeholder="Content"
-                ></textarea>
-                <button onClick={() => handleSave(post.id)}>Save</button>
-                <button onClick={handleCancel}>Cancel</button>
-              </div>
-            ) : (
-              <div>
-                <h3>{post.title}</h3>
-                <p>{post.content}</p>
-                <div className="post-actions">
-                  <button onClick={() => handleEdit(post.id)}>Edit</button>
-                  <button onClick={() => handleDelete(post.id)}>Delete</button>
+    <div className="background-set">
+      <div className="community-container">
+        <h2>Community Content</h2>
+        <div className="posts-container">
+          {communityData.map((post) => (
+            <div key={post.id} className="post">
+              {editingId === post.id ? (
+                <div className="edit-post">
+                  <input
+                    type="text"
+                    value={newPostTitle}
+                    onChange={(e) => setNewPostTitle(e.target.value)}
+                    placeholder="Title"
+                  />
+                  <textarea
+                    value={newPostContent}
+                    onChange={(e) => setNewPostContent(e.target.value)}
+                    placeholder="Content"
+                  ></textarea>
+                  <button onClick={() => handleSave(post.id)}>Save</button>
+                  <button onClick={handleCancel}>Cancel</button>
                 </div>
-              </div>
-            )}
-          </div>
-        ))}
+              ) : (
+                <div>
+                  <h3>{post.title}</h3>
+                  <p>{post.content}</p>
+                  <div className="post-actions">
+                    <button onClick={() => handleEdit(post.id)}>Edit</button>
+                    <button onClick={() => handleDelete(post.id)}>
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+        <div className="new-post-container">
+          <h3>Add New Post</h3>
+          <input
+            type="text"
+            value={newPostTitle}
+            onChange={(e) => setNewPostTitle(e.target.value)}
+            placeholder="Title"
+          />
+          <textarea
+            value={newPostContent}
+            onChange={(e) => setNewPostContent(e.target.value)}
+            placeholder="Content"
+          ></textarea>
+          <button onClick={handleAdd}>Add Post</button>
+        </div>
       </div>
-      <div className="new-post-container">
-        <h3>Add New Post</h3>
-        <input
-          type="text"
-          value={newPostTitle}
-          onChange={(e) => setNewPostTitle(e.target.value)}
-          placeholder="Title"
-        />
-        <textarea
-          value={newPostContent}
-          onChange={(e) => setNewPostContent(e.target.value)}
-          placeholder="Content"
-        ></textarea>
-        <button onClick={handleAdd}>Add Post</button>
-      </div>
-    </div>
     </div>
   );
 };
